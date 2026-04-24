92814069e85f69a3016eb610b50efc1a61dd41a5
keincarrillo <kein.carrillo.escom@gmail.com>
2026-04-23 22:50:25 -0600

    feat: add Contact and Testimonials components and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useLenis } from './hooks/useLenis'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Services from './components/Services/Services'
+import About from './components/About/About'
+import Team from './components/Team/Team'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
 const App = () => {
+  useLenis()
+
   return (
     <>
-      <section className="h-screen bg-indigo-500 flex items-center justify-center">
-        <h1>Hola mundo </h1>
-      </section>
+      <Navbar />
+      <main>
+        <Hero />
+        <Services />
+        <About />
+        <Team />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
     </>
   )
 }
