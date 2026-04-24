f228b29cf123e63e8ddf2e7996b58aa5fae0993b
keincarrillo <kein.carrillo.escom@gmail.com>
2026-04-23 22:49:06 -0600

    feat: add useLenis and useScrollReveal hooks for smooth scrolling and reveal animations

---
+++ src/hooks/useLenis.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export const useLenis = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.3,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true
+    })
+
+    // mantiene ScrollTrigger sincronizado con Lenis
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add(time => {
+      lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+}
