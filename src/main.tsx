7d2dcd6ddfaa8ad8404d21de92c4872741251863
keincarrillo <kein.carrillo.escom@gmail.com>
2026-06-29 23:32:31 -0600

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import './index.css'
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
